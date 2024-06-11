51e33eccb3cd0165f9b1268890afe793291f5f2e
Joanna <joanna_pinczuk@hotmail.com>
2024-06-11 23:45:29 +0200

    updates

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,22 @@
+import React, { useState } from "react";
+
 function App() {
+  const [feedback, setFeedback] = useState({ 
+    good: 0, 
+    neutral: 0, 
+    bad: 0 });
+  const handleFeedback = (feedbackType) =>
+    {setFeedback(prevFeedback =>({
+      ...prevFeedback, 
+      [feedbackType]:prevFeedback[feedbackType] + 1
+    }));
+  }
   return (
     <>
-      <h1>React Homework Template (Vite)</h1>
+      <h1>Espresso Feedback</h1>
+      <div>
+        <h2>Please leave your feedback</h2>
+      </div>
     </>
   );
 }
